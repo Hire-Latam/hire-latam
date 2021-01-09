ca178be7676a867eda1d7bcd9ddaeac8f7935ef6
Alberto Romero <lguitarras0594@gmail.com>
2021-01-09 17:38:08 -0600

    Add apply now button

--- pages/index.vue
+++ pages/index.vue
@@ -7,30 +7,33 @@
       We are seeing the gap between practices in Silicon Valley and Latin
       America is becoming closer" - Bismarck Lepe
     </p>
-    <h3 class="mt-3 text-xl text-gray-300">Jobs</h3>
+    <h3 class="mt-3 text-xl text-gray-50">Jobs</h3>
     <div
       v-for="job in jobs"
       :key="job.id"
-      class="flex items-center border-2 rounded-2xl border-solid border-white p-2 mt-2 cursor-pointer hover:bg-gray-900"
+      class="flex justify-between items-center border-2 rounded-2xl border-solid border-white p-2 mt-2 cursor-pointer hover:bg-gray-900"
     >
-      <div class="hidden md:flex rounded-full overflow-hidden w-16 h-16">
-        <img class="object-cover" src="/logo.png" />
-      </div>
-      <div class="flex flex-col justify-between mx-4">
-        <div>
-          <h1 class="text-xl text-gray-50">{{ job.title }}</h1>
-          <h2 class="text-sm text-gray-300">{{ job.company }}</h2>
+      <div class="flex">
+        <div class="hidden md:flex rounded-full overflow-hidden w-16 h-16">
+          <img class="object-cover" src="/logo.png" />
         </div>
-        <ul class="flex flex-wrap mt-2">
-          <li
-            class="text-xs mr-3 bg-gray-50 rounded-3xl m-1 py-1 px-3"
-            v-for="tag in job.tags"
-            :key="`${job.id}-${tag}`"
-          >
-            {{ tag }}
-          </li>
-        </ul>
+        <div class="flex flex-col justify-between mx-4">
+          <div>
+            <h1 class="text-xl text-gray-50">{{ job.title }}</h1>
+            <h2 class="text-sm text-gray-300">{{ job.company }}</h2>
+          </div>
+          <ul class="flex flex-wrap mt-2">
+            <li
+              class="text-xs mr-3 bg-gray-50 rounded-3xl m-1 py-1 px-3"
+              v-for="tag in job.tags"
+              :key="`${job.id}-${tag}`"
+            >
+              {{ tag }}
+            </li>
+          </ul>
+        </div>
       </div>
+      <button class="mr-3 p-3 bg-gray-50 rounded-lg text-sm">Apply now</button>
     </div>
   </div>
 </template>
