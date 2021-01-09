2c44aec4e1f7537a6e6a393c11eb78f42c103b86
Alan <alanmunguiacerda@hotmail.com>
2021-01-09 16:41:47 -0600

    Fix image position

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,22 @@
     <div
       v-for="job in jobs"
       :key="job.id"
-      class="border-2 rounded-2xl border-solid border-white flex mt-3"
+      class="flex items-center border-2 rounded-2xl border-solid border-white p-2"
     >
-      <div class="flex">
-        <img src="https://placehold.it/150x150" />
+      <div class="flex rounded-lg overflow-hidden h-16 w-16">
+        <img class="object-cover" src="https://placehold.it/150x150" />
       </div>
-      <div class="flex flex-col justify-between my-2 mx-4">
+      <div class="flex flex-col justify-between mx-4">
         <div>
           <h1 class="text-3xl text-gray-50">{{ job.title }}</h1>
           <h2 class="text-xl text-gray-300">{{ job.company }}</h2>
         </div>
         <ul class="flex mt-2">
-          <li class="mr-3 bg-gray-50 rounded-3xl p-1 px-3" v-for="tag in job.tags" :key="`${job.id}-${tag}`">
+          <li
+            class="mr-3 bg-gray-50 rounded-3xl p-1 px-3"
+            v-for="tag in job.tags"
+            :key="`${job.id}-${tag}`"
+          >
             {{ tag }}
           </li>
         </ul>
