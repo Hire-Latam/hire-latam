aa4c07a5ded4d372a2eead1411a721e07c9c0985
Alberto Romero <lguitarras0594@gmail.com>
2021-01-09 18:19:57 -0600

    Add Sections Title to page

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,13 @@
     class="flex flex-col items-center h-full min-h-screen p-4 font-serif bg-primary"
   >
     <div class="w-full max-w-screen-lg">
-      <h1 class="text-3xl text-gray-50">Hire Latam</h1>
+      <div class="flex justify-between">
+        <h1 class="text-3xl text-gray-50">Hire Latam</h1>
+        <ul class="flex flex-row items-end">
+          <li class="text-gray-50 ml-3 hover:underline cursor-pointer"> Latam </li>
+          <li class="text-gray-50 ml-3 hover:underline cursor-pointer"> Contact Us </li>
+        </ul>
+      </div>
       <h3 class="mt-3 text-xl text-gray-50">Jobs</h3>
       <div class="flex flex-col">
         <job-item v-for="job in jobs" :key="job.id" :job="job" />
