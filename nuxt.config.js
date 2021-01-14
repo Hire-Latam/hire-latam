25555c8b96e3cff990962f469dd7181f3407d92d
Alberto Romero <lguitarras0594@gmail.com>
2021-01-13 19:46:59 -0600

    Add nuxt config for firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,5 +15,23 @@ export default {
     link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins&display=swap' }],
   },
   buildDir: 'nuxt-dist',
-  buildModules: ['@nuxtjs/tailwindcss']
+  buildModules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: {
+            apiKey: "AIzaSyCpB0-I0ZwuJODKBhzmDFcj2EHwLQV6wFo",
+            authDomain: "hire-latam.firebaseapp.com",
+            projectId: "hire-latam",
+            storageBucket: "hire-latam.appspot.com",
+            messagingSenderId: "1062476652280",
+            appId: "1:1062476652280:web:b2479c70eef4b33c23650d",
+            measurementId: "G-0GLY8MBQL0"
+          }
+        }
+      }
+    ]
+  ],
 }
