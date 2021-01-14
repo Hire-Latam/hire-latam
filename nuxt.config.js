de07f08442bd7375a5c967e6fea8b7ba09915e97
Alberto Romero <lguitarras0594@gmail.com>
2021-01-13 20:15:14 -0600

    Add databaseURL for firebase config

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
         config: {
           apiKey: process.env.FIREBASE_API_KEY,
           authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: `https://${process.env.FIREBASE_DATABASE_NAME}.firebaseio.com`,
           projectId: process.env.FIREBASE_PROJECT_ID,
           storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
           messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
