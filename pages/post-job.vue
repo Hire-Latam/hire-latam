affaef2fbd45904b20c5941257fc839cc927de4b
Alberto Romero <lguitarras0594@gmail.com>
2021-01-23 15:52:19 -0600

    fix coloring due to black mode enabled in local machine

--- pages/post-job.vue
+++ pages/post-job.vue
@@ -2,26 +2,26 @@
   <div class="flex flex-col items-center h-full min-h-screen p-4">
     <div class="w-full max-w-screen-lg relative">
       <navigation active-page="post" />
-      <p class="mt-3">
+      <p class="mt-3 text-white">
         Get exposure by publishing what you are looking for in our 2-minutes
         form. Really simple!
       </p>
       <form class="mt-3">
         <div class="flex flex-col">
-          <label> Job Title </label>
-          <input class="p-1" type="text" name="job-title" />
-          <label> Company Name </label>
-          <input class="p-1" type="text" name="company-name" />
-          <label> Description </label>
-          <textarea class="p-1" name="description" />
-          <label> Salary Min </label>
-          <input class="p-1" type="number" name="salary-min" />
-          <label> Salary Max </label>
-          <input class="p-1" type="number" name="salary-max" />
+          <label class="text-white"> Job Title </label>
+          <input class="p-1 bg-gray-200" type="text" name="job-title" />
+          <label class="text-white"> Company Name </label>
+          <input class="p-1 bg-gray-200" type="text" name="company-name" />
+          <label class="text-white"> Description </label>
+          <textarea class="p-1 bg-gray-200" name="description" />
+          <label class="text-white"> Salary Min </label>
+          <input class="p-1 bg-gray-200" type="number" name="salary-min" />
+          <label class="text-white"> Salary Max </label>
+          <input class="p-1 bg-gray-200" type="number" name="salary-max" />
           <!-- Los tags, la wea :'v -->
         </div>
         <button
-          class="mt-3 bg-gray-900 align-right px-4 py-2 rounded-sm"
+          class="mt-3 bg-gray-200 align-right px-4 py-2 rounded-sm text-primary"
           type="submit"
         >
           Post
