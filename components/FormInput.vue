43f767475db068543782902a824928cdc49c09b5
Alan <alanmunguiacerda@hotmail.com>
2021-01-23 17:23:21 -0600

    Add FormInput component

---
+++ components/FormInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col flex-grow my-2">
+    <label class="text-white">
+      {{ label }}
+      <span v-if="required">*</span>
+    </label>
+    <input
+      class="flex p-2 bg-gray-200 rounded-md"
+      :type="type"
+      :name="name"
+      :placeholder="placeholder"
+      :required="required"
+      :min="min"
+      :max="max"
+      @value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: [
+    "label",
+    "type",
+    "name",
+    "placeholder",
+    "required",
+    "value",
+    "min",
+    "max",
+  ],
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,25 @@
     <div class="w-full max-w-screen-lg relative">
       <navigation active-page="home" />
       <h3 class="mt-3 text-xl text-white">Jobs</h3>
-      <search v-model="terms" @change="handleInputChange" />
+      <div class="w-1/3">
+        <form-input v-model.lazy="terms" placeholder="Search keywords" />
+      </div>
       <jobs-list :terms="terms" />
     </div>
   </div>
 </template>
 
 <script>
-import JobsList from "../components/JobsList.vue";
-import Search from "../components/Search.vue";
 import Navigation from "../components/Navigation.vue";
+import FormInput from "../components/FormInput.vue";
+import JobsList from "../components/JobsList.vue";
 
 export default {
-  components: { JobsList, Navigation, Search },
-  data: function () {
+  components: { Navigation, FormInput, JobsList },
+  data: function() {
     return {
       terms: "",
     };
-  },
-  methods: {
-    handleInputChange: function (terms) {
-      this.terms = terms;
-    },
   },
 };
 </script>
